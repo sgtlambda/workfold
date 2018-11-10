eeafc5461b09f826745273bd3d832135d000fa9d
JM Versteeg <janmerijnversteeg@gmail.com>
2018-11-10 21:28:44 +0100

    faster test

--- test.js
+++ test.js
@@ -8,11 +8,11 @@ const handler = (i, initialWait = 0) => async () => {
     console.log(`[${i}] Before delay`);
     await delay(initialWait);
     console.log(`[${i}] Before mutex`);
-    await mutex(() => delay(2500), 'work');
+    await mutex(() => delay(100), 'work');
     console.log(`[${i}] After mutex`);
 };
 
 handler(0)();
-handler(1, 750)();
-handler(2, 1500)();
-handler(3, 3000)();
+handler(1, 10)();
+handler(2, 20)();
+handler(3, 250)();
